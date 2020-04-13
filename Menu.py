40a8d98bafc9dddcb69ab07827209b6401e770f5
normandy14 <normandy14@protonmail.com>
2020-04-13 12:42:20 -0400

    Refactor menu class into model, view, and controller classes

--- Menu.py
+++ Menu.py
@@ -3,39 +3,53 @@ import sys
 import getpass
 import duolingo
 
-class Menu:
-    """Class to interact with 'view' of the program"""
+class Controller:
+    def __init__(self):
+        self.model = Model()
+        self.view = View()
     
+    def run(self):
+        flag = self.view.display()
+        if flag == True:
+            self.model.userCredentials()
+            self.model.signIn()
+        else:
+            self.exit()
+    
+    def exit(self):
+        print ("until next time...")
+        sys.exit()
+        
+
+class Model:
     def __init__(self):
         self.username = None
         self.password = None
         self.session = None
-        
-    def displayScreen(self):
-        """Initial prompt to continue or exit program"""
-        
-        print ("Welcome to duo terminal: review duolingo words on your terminal")
-        resp = (input("Select: (l)ogIn or (q)uit \n")).lower()
-        if resp== "l":
-            self.getCredentials()
-            self.logIn()
-        elif resp == "q":
-            print ("until next time!")
-            sys.exit()
     
-    def getCredentials(self):
-        """Prompts the user for the username and password of the duolingo account"""
-        
+    def userCredentials(self):
         print ("Enter Credentials: ")
-        self.username = getpass.getpass("username: ") # getpass records securely sensitive user information
+        self.username = getpass.getpass("username: ")
         self.password = getpass.getpass("password: ")
     
-    def logIn(self):
-        """Uses username and password supplied by user to log into Duolingo and records it into a 'session'"""
-        
+    def signIn(self):
         print ("signing into duolingo with credentials... ")
         self.session = duolingo.Duolingo(self.username, self.password)
         print (self.session.get_user_info())
+    
+    
+class View:
+    def __init__(self):
+        pass
+        
+    def display(self):
+        print ("welcome to duo terminal: review duolingo words on your terminal")
+        resp = (input("select: (l)ogIn or (q)uit \n")).lower()
+        if resp== "l":
+            return True
+        elif resp == "q":
+            return False
+    
 
-menu = Menu()
-menu.displayScreen()
+app = Controller()
+app.run()
